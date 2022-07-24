17d609e5528edc3b07f1e813addadab3467a69c6
neyhuan555 <neyhuansikoko18@gmail.com>
2022-07-24 12:32:06 +0700

    Hook up date picker to edit text for expiration date

--- app/src/main/java/com/neyhuansikoko/warrantylogger/AddWarrantyFragment.kt
+++ app/src/main/java/com/neyhuansikoko/warrantylogger/AddWarrantyFragment.kt
@@ -1,12 +1,15 @@
 package com.neyhuansikoko.warrantylogger
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.datepicker.MaterialDatePicker
 import com.neyhuansikoko.warrantylogger.databinding.FragmentAddWarrantyBinding
 import com.neyhuansikoko.warrantylogger.databinding.FragmentFirstBinding
+import java.text.SimpleDateFormat
 
 class AddWarrantyFragment : Fragment() {
 
@@ -23,6 +26,25 @@ class AddWarrantyFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentAddWarrantyBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            tilEtExpirationDate.setOnClickListener {
+                val datePicker =
+                    MaterialDatePicker.Builder.datePicker()
+                        .setTitleText("Select date")
+                        .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                        .build()
+                datePicker.apply {
+                    addOnPositiveButtonClickListener { date ->
+                        binding.tilEtExpirationDate.setText(SimpleDateFormat("yyyy/MM/dd").format(date))
+                    }
+                    show(this@AddWarrantyFragment.requireActivity().supportFragmentManager, "tag")
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
